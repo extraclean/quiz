350ac9682c75d22611806349a1ee57f6af22d888
extraclean <alexcheng1005@outlook.com>
2015-10-17 00:40:27 +0800

    finish quiz 2 iterative with fixed size input scale
    input:
    	first line is the input array
    	second line is the input character

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,38 @@
 /* FIXME: Implement! */
+#include"stdio.h"
+#include"stdlib.h"
+
+char smallest_character(char*, char);
 
 int main()
 {
+    FILE *fp;
+    char *str, c;
+
+    fp = fopen("input.txt", "r");
+    if(fp == NULL) {
+        printf("input error\n");
+        return -1;
+    }
+        fscanf(fp, "%c %c %c %c %c\n %c", &str[0], &str[1], &str[2], &str[3], &str[4], &c);
+    c = smallest_character(str,c);
+    printf("%c\n", c);
+    fclose(fp);
     return 0;
+}
+
+char smallest_character(char str[], char c)
+{
+    int i = 0;
+    while(str[i] != NULL) {
+        if(str[i]>c) {
+            c = str[i];
+            break;
+        } else
+            i++;
+    }
+    if(str[i] == NULL) {
+        c = str[0];
+    }
+    return c;
 }
