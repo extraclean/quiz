ff06e8f209f5b79b93de891ab6a980ca9af94600
extraclean <alexcheng1005@outlook.com>
2015-10-17 21:44:56 +0800

    forget to astyle q3

--- q3/recursive.c
+++ q3/recursive.c
@@ -1,69 +1,71 @@
 #include"stdio.h"
 
-typedef struct tree{
-	int val;
-	struct tree *left;
-	struct tree *right;
-}TreeNode;
+typedef struct tree {
+    int val;
+    struct tree *left;
+    struct tree *right;
+} TreeNode;
 
-TreeNode *initial(){
-	TreeNode *root = (TreeNode *)malloc(sizeof(TreeNode));
-	TreeNode *l = (TreeNode *)malloc(sizeof(TreeNode));
-	TreeNode *ll = (TreeNode *)malloc(sizeof(TreeNode));
-	TreeNode *lr = (TreeNode *)malloc(sizeof(TreeNode));
-	TreeNode *r = (TreeNode *)malloc(sizeof(TreeNode));
-	TreeNode *rr = (TreeNode *)malloc(sizeof(TreeNode));
+TreeNode *initial()
+{
+    TreeNode *root = (TreeNode *)malloc(sizeof(TreeNode));
+    TreeNode *l = (TreeNode *)malloc(sizeof(TreeNode));
+    TreeNode *ll = (TreeNode *)malloc(sizeof(TreeNode));
+    TreeNode *lr = (TreeNode *)malloc(sizeof(TreeNode));
+    TreeNode *r = (TreeNode *)malloc(sizeof(TreeNode));
+    TreeNode *rr = (TreeNode *)malloc(sizeof(TreeNode));
 
-	root->val = 1;
-	l->val = 2;
-	ll->val = 3;
-	lr->val = 4;
-	r->val = 5;
-	rr->val = 6;
+    root->val = 1;
+    l->val = 2;
+    ll->val = 3;
+    lr->val = 4;
+    r->val = 5;
+    rr->val = 6;
 
-	root->left = l;
-	root->right = r;
-	l->left = ll;
-	l->right = lr;
-	ll->left = NULL;
-	ll->right = NULL;
-	lr->left = NULL;
-	lr->right = NULL;
-	r->left = NULL;
-	r->right = rr;
-	rr->left = NULL;
-	rr->right = NULL;
+    root->left = l;
+    root->right = r;
+    l->left = ll;
+    l->right = lr;
+    ll->left = NULL;
+    ll->right = NULL;
+    lr->left = NULL;
+    lr->right = NULL;
+    r->left = NULL;
+    r->right = rr;
+    rr->left = NULL;
+    rr->right = NULL;
 
-	return root;
+    return root;
 }
 
-void flatten(TreeNode *root) {
-	
-	TreeNode *cur = root;
-        if(cur){
-		TreeNode *right = cur->right;
-		if(cur->left) {
-			TreeNode *tmp = cur->left;
-			while(tmp->right) {
-				tmp = tmp->right;
-			}
-			tmp->right = right;
-			cur->right = cur->left;
-			cur->left = NULL;
-		}
-		printf("%d->", cur->val);
-		cur = cur->right;
-		flatten(cur);
-	}
+void flatten(TreeNode *root)
+{
+
+    TreeNode *cur = root;
+    if(cur) {
+        TreeNode *right = cur->right;
+        if(cur->left) {
+            TreeNode *tmp = cur->left;
+            while(tmp->right) {
+                tmp = tmp->right;
+            }
+            tmp->right = right;
+            cur->right = cur->left;
+            cur->left = NULL;
+        }
+        printf("%d->", cur->val);
+        cur = cur->right;
+        flatten(cur);
+    }
 }
 
 int main()
 {
-	TreeNode *root = initial();
-	printf("Output: ");
-	flatten(root);
-	printf("null\n");
-	
+    TreeNode *root = initial();
+    printf("Output: ");
+    flatten(root);
+    printf("null\n");
+
     return 0;
 }
 
