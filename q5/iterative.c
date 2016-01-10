d0b770eb67b243680a1e69781e9fa954af547fad
extraclean <alexcheng1005@outlook.com>
2016-01-11 00:47:37 +0800

    finish simple version of iterative.c in q5

--- q5/iterative.c
+++ q5/iterative.c
@@ -1,6 +1,76 @@
-/* FIXME: Implement! */
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct list {
+	int val;
+	struct list *next;
+}ListNode;
+
+int detectCycle(ListNode *head) {
+	if(!head)
+		return 0;
+	ListNode *p=head;
+	ListNode *q=head;
+
+	while (1){
+		if(p->next!=NULL)
+			p=p->next;
+		else
+			return 0;
+		if(q->next!=NULL && q->next->next!=NULL)
+			q=q->next->next;
+		else
+			return 0;
+		if (p==q){
+			q=head;
+			while (p!=q){
+				p=p->next;
+				q=q->next;
+			}
+			return p->val;
+		}
+	}
+}
 
 int main()
 {
-    return 0;
+	ListNode *input = (ListNode *)malloc(sizeof(ListNode));
+	ListNode *input2 = (ListNode *)malloc(sizeof(ListNode));
+	ListNode *input3 = (ListNode *)malloc(sizeof(ListNode));
+	ListNode *input4 = (ListNode *)malloc(sizeof(ListNode));
+	ListNode *input5 = (ListNode *)malloc(sizeof(ListNode));
+	ListNode *input6 = (ListNode *)malloc(sizeof(ListNode));
+
+	input->val=1;
+	input2->val=2;
+	input3->val=3;
+	input4->val=4;
+	input5->val=5;
+	input6->val=6;
+
+	input->next=input2;
+	input2->next=input3;
+	input3->next=input4;
+	input4->next=input5;	
+	input5->next=input6;	
+	input6->next=input2;
+
+	printf("input: ");
+	printf("%d",input->val);//1
+	printf("->");
+	printf("%d",input->next->val);//2
+	printf("->");
+	printf("%d",input->next->next->val);//3
+	printf("->");
+	printf("%d",input->next->next->next->val);//4
+	printf("->");
+	printf("%d",input->next->next->next->next->val);//5
+	printf("->");
+	printf("%d",input->next->next->next->next->next->val);//6
+	printf("->");
+	printf("%d\n",input->next->val);//2
+
+	printf("result: ");
+	printf("%d\n",detectCycle(input));
+	return 0;
 }
